10648a0716100d1ec2bce29b746a7aad62c890c6
Long Nguyen <nguyen.long.908132@gmail.com>
2025-02-02 23:46:57 +0700

    Add `srt` ingest mode

---
+++ src/modules/stream/ffmpegIngest.ts
@@ -0,0 +1,51 @@
+import ffmpeg from "fluent-ffmpeg";
+import { PassThrough } from "node:stream";
+
+function randomInclusive(min: number, max: number) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+export function ffmpegIngest(port?: number) {
+    port ??= randomInclusive(30000, 40000);
+    const host = `srt://localhost:${port}`;
+    const output = new PassThrough();
+    const command = ffmpeg(host)
+        .on("stderr", (line) => console.log(line))
+        .addOption(
+            '-fflags', 'nobuffer',
+            "-fflags", "flush_packets",
+            "-flags", "low_delay",
+            "-buffer_size", "4194304",
+            "-err_detect", "ignore_err",
+            "-thread_queue_size", "4096",
+            "-flush_packets", "1",
+            "-stats",
+        )
+        .addInputOption(
+            "-analyzeduration", "1500000",
+            "-mode", "listener"
+        )
+        .output(output)
+        .outputFormat("matroska");
+
+    command.addOutputOption("-map 0:v");
+    command.videoCodec("copy");
+
+    command
+        .addOutputOption("-map 0:a?")
+        .audioChannels(2)
+        /*
+         * I don't have much surround sound material to test this with,
+         * if you do and you have better settings for this, feel free to
+         * contribute!
+         */
+        .addOutputOption("-lfe_mix_level 1")
+        .audioFrequency(48000)
+        .audioCodec("libopus")
+        .audioBitrate(`128k`);
+    
+    command.run();
+    return { command, output, host }
+}
--- src/modules/stream/index.ts
+++ src/modules/stream/index.ts
@@ -1,7 +1,8 @@
 import { Client, StageChannel } from "discord.js-selfbot-v13";
 import { Command } from "@commander-js/extra-typings";
-import { Streamer, type StreamOptions } from "@dank074/discord-video-stream";
+import { Streamer } from "@dank074/discord-video-stream";
 import { NewApi } from "@dank074/discord-video-stream";
+import { ffmpegIngest } from "./ffmpegIngest.js";
 
 import { createCommand } from "../index.js";
 import type { Module } from "../index.js";
@@ -63,6 +64,69 @@ export default {
               forceChacha20Encryption: true,
               readrateInitialBurst: opts.livestream ? 10 : undefined
             })
+          }
+          catch (e)
+          {
+            const error = e as Error;
+            message.reply(
+              `Oops, something bad happened
+\`\`\`
+${error.message}
+\`\`\``
+            )
+          }
+        }
+      ),
+
+      createCommand(
+        new Command("obs")
+          .option("--room <id>", "The room ID, specified as <guildId>/<channelId>. If not specified, use the current room of the caller"),
+        async (message, args, opts) => {
+          const url = args[0];
+          let guildId: string, channelId: string
+          if (opts.room) {
+            [guildId, channelId] = opts.room.split("/");
+            if (!guildId) {
+              message.reply("Guild ID is empty");
+              return;
+            }
+            if (!channelId) {
+              message.reply("Channel ID is empty");
+              return;
+            }
+          }
+          else {
+            guildId = message.guildId!;
+            const channelIdNullable = message.author.voice?.channel?.id;
+            if (!channelIdNullable) {
+              message.reply("Please join a voice channel first!");
+              return;
+            }
+            channelId = channelIdNullable;
+          }
+          playback?.kill("SIGTERM");
+          try {
+            if (
+              !streamer.voiceConnection ||
+              streamer.voiceConnection.guildId !== guildId ||
+              streamer.voiceConnection.channelId !== channelId
+            )
+              await streamer.joinVoice(guildId, channelId);
+
+            if (streamer.client.user!.voice!.channel instanceof StageChannel)
+              await streamer.client.user!.voice!.setSuppressed(false);
+
+            const { command, output, host } = ffmpegIngest();
+            playback = command;
+
+            message.reply(`Please connect your OBS to \`${host}\``);
+            output.once("data", () => {
+              message.reply("Media stream found. Starting playback...");
+            })
+            await NewApi.playStream(output, streamer, {
+              forceChacha20Encryption: true,
+              readrateInitialBurst: 10
+            });
           }
           catch (e)
           {
