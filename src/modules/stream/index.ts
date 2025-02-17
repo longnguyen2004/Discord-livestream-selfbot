0e4efb8b1a9731404ff2f08563cd284694d795fb
Long Nguyen <nguyen.long.908132@gmail.com>
2025-02-17 23:33:46 +0700

    Add yt-dlp, reformat

--- src/modules/stream/index.ts
+++ src/modules/stream/index.ts
@@ -2,183 +2,201 @@ import { Command, Option } from "@commander-js/extra-typings";
 import { Streamer } from "@dank074/discord-video-stream";
 import { prepareStream, playStream } from "@dank074/discord-video-stream";
 import * as Ingestor from "./ffmpegIngest.js";
+import * as ytdlp from "./yt-dlp.js";
 import { createCommand } from "../index.js";
 import { StageChannel } from "discord.js-selfbot-v13";
 
 import type { Module } from "../index.js";
 import type Ffmpeg from "fluent-ffmpeg";
+import type { Message } from "discord.js-selfbot-v13";
 
-function ffmpegErrorHandler(err: Error, stdout: string | null, stderr: string | null)
-{
-  if (/SIG(TERM|KILL)/.test(err.message))
-    return;
-  console.log("FFmpeg encountered an error");
-  console.log(err);
+function ffmpegErrorHandler(
+	err: Error,
+	stdout: string | null,
+	stderr: string | null,
+) {
+	if (/SIG(TERM|KILL)/.test(err.message)) return;
+	console.log("FFmpeg encountered an error");
+	console.log(err);
+}
+
+async function joinRoom(
+	streamer: Streamer,
+	message: Message,
+	optionalRoom?: string,
+) {
+	let guildId: string, channelId: string;
+	if (optionalRoom) {
+		[guildId, channelId] = optionalRoom.split("/");
+		if (!guildId) {
+			message.reply("Guild ID is empty");
+			return false;
+		}
+		if (!channelId) {
+			message.reply("Channel ID is empty");
+			return false;
+		}
+	} else {
+		guildId = message.guildId!;
+		const channelIdNullable = message.author.voice?.channel?.id;
+		if (!channelIdNullable) {
+			message.reply("Please join a voice channel first!");
+			return false;
+		}
+		channelId = channelIdNullable;
+	}
+	if (
+		!streamer.voiceConnection ||
+		streamer.voiceConnection.guildId !== guildId ||
+		streamer.voiceConnection.channelId !== channelId
+	)
+		await streamer.joinVoice(guildId, channelId);
+
+	if (streamer.client.user!.voice!.channel instanceof StageChannel)
+		await streamer.client.user!.voice!.setSuppressed(false);
 }
 
 export default {
-  name: "stream",
-  register(bot) {
-    const streamer = new Streamer(bot.client, {
-      forceChacha20Encryption: true
-    });
-    let playback: Ffmpeg.FfmpegCommand;
-    return [
-      createCommand(
-        new Command("play")
-          .description("Play a video file or link")
-          .argument("<url>", "The url to play")
-          .option("--copy", "Copy the stream directly instead of re-encoding")
-          .option("--livestream", "Specify if the stream is a livestream")
-          .option("--room <id>", "The room ID, specified as <guildId>/<channelId>. If not specified, use the current room of the caller"),
-        async (message, args, opts) => {
-          const url = args[0];
-          let guildId: string, channelId: string
-          if (opts.room) {
-            [guildId, channelId] = opts.room.split("/");
-            if (!guildId) {
-              message.reply("Guild ID is empty");
-              return;
-            }
-            if (!channelId) {
-              message.reply("Channel ID is empty");
-              return;
-            }
-          }
-          else {
-            guildId = message.guildId!;
-            const channelIdNullable = message.author.voice?.channel?.id;
-            if (!channelIdNullable) {
-              message.reply("Please join a voice channel first!");
-              return;
-            }
-            channelId = channelIdNullable;
-          }
-          playback?.kill("SIGTERM");
-          try {
-            if (
-              !streamer.voiceConnection ||
-              streamer.voiceConnection.guildId !== guildId ||
-              streamer.voiceConnection.channelId !== channelId
-            )
-              await streamer.joinVoice(guildId, channelId);
+	name: "stream",
+	register(bot) {
+		const streamer = new Streamer(bot.client, {
+			forceChacha20Encryption: true,
+		});
+		let playback: Ffmpeg.FfmpegCommand;
+		return [
+			createCommand(
+				new Command("play")
+					.description("Play a video file or link")
+					.argument("<url>", "The url to play")
+					.option("--copy", "Copy the stream directly instead of re-encoding")
+					.option("--livestream", "Specify if the stream is a livestream")
+					.option(
+						"--room <id>",
+						"The room ID, specified as <guildId>/<channelId>. If not specified, use the current room of the caller",
+					),
+				async (message, args, opts) => {
+					const url = args[0];
+					await joinRoom(streamer, message, opts.room);
+					playback?.kill("SIGTERM");
+					try {
+						const { command, output } = prepareStream(url, {
+							noTranscoding: !!opts.copy,
+						});
+						command.on("error", ffmpegErrorHandler);
+						playback = command;
 
-            if (streamer.client.user!.voice!.channel instanceof StageChannel)
-              await streamer.client.user!.voice!.setSuppressed(false);
-
-            const { command, output } = prepareStream(url, {
-              noTranscoding: !!opts.copy
-            });
-            command.on("error", ffmpegErrorHandler);
-            playback = command;
-
-            await playStream(output, streamer, {
-              readrateInitialBurst: opts.livestream ? 10 : undefined
-            })
-          }
-          catch (e)
-          {
-            const error = e as Error;
-            message.reply(
-              `Oops, something bad happened
+						await playStream(output, streamer, {
+							readrateInitialBurst: opts.livestream ? 10 : undefined,
+						});
+					} catch (e) {
+						const error = e as Error;
+						message.reply(
+							`Oops, something bad happened
 \`\`\`
 ${error.message}
-\`\`\``
-            )
-          }
-        }
-      ),
+\`\`\``,
+						);
+					}
+				},
+			),
 
-      createCommand(
-        new Command("obs")
-          .description("Starts an OBS ingest server for livestreaming")
-          .option("--room <id>", "The room ID, specified as <guildId>/<channelId>. If not specified, use the current room of the caller")
-          .option("-p, --port <port>", "Port to use, leave blank for a random port", Number.parseInt)
-          .addOption(
-            new Option("--protocol <prot>", "Stream protocol to use")
-              .choices(["rtmp", "srt", "rist"])
-              .default("srt")
-          ),
-        async (message, args, opts) => {
-          let guildId: string, channelId: string
-          if (opts.room) {
-            [guildId, channelId] = opts.room.split("/");
-            if (!guildId) {
-              message.reply("Guild ID is empty");
-              return;
-            }
-            if (!channelId) {
-              message.reply("Channel ID is empty");
-              return;
-            }
-          }
-          else {
-            guildId = message.guildId!;
-            const channelIdNullable = message.author.voice?.channel?.id;
-            if (!channelIdNullable) {
-              message.reply("Please join a voice channel first!");
-              return;
-            }
-            channelId = channelIdNullable;
-          }
-          playback?.kill("SIGTERM");
-          try {
-            if (
-              !streamer.voiceConnection ||
-              streamer.voiceConnection.guildId !== guildId ||
-              streamer.voiceConnection.channelId !== channelId
-            )
-              await streamer.joinVoice(guildId, channelId);
+			createCommand(
+				new Command("obs")
+					.description("Starts an OBS ingest server for livestreaming")
+					.option(
+						"--room <id>",
+						"The room ID, specified as <guildId>/<channelId>. If not specified, use the current room of the caller",
+					)
+					.option(
+						"-p, --port <port>",
+						"Port to use, leave blank for a random port",
+						Number.parseInt,
+					)
+					.addOption(
+						new Option("--protocol <prot>", "Stream protocol to use")
+							.choices(["rtmp", "srt", "rist"])
+							.default("srt"),
+					),
+				async (message, args, opts) => {
+					await joinRoom(streamer, message, opts.room);
+					playback?.kill("SIGTERM");
+					try {
+						const ingestor = {
+							srt: Ingestor.ingestSrt,
+							rtmp: Ingestor.ingestRtmp,
+							rist: Ingestor.ingestRist,
+						} as const;
+						const { command, output, host } = ingestor[opts.protocol](
+							opts.port,
+						);
 
-            if (streamer.client.user!.voice!.channel instanceof StageChannel)
-              await streamer.client.user!.voice!.setSuppressed(false);
+						command.on("error", ffmpegErrorHandler);
+						command.on("stderr", (line) => console.log(line));
+						playback = command;
 
-            const ingestor = {
-              srt: Ingestor.ingestSrt,
-              rtmp: Ingestor.ingestRtmp,
-              rist: Ingestor.ingestRist
-            } as const;
-            const { command, output, host } = ingestor[opts.protocol](opts.port);
-            
-            command.on("error", ffmpegErrorHandler);
-            command.on("stderr", (line) => console.log(line));
-            playback = command;
-
-            message.reply(`Please connect your OBS to \`${host}\``);
-            output.once("data", () => {
-              message.reply("Media stream found. Starting playback...");
-            })
-            await playStream(output, streamer, {
-              readrateInitialBurst: 10
-            });
-          }
-          catch (e)
-          {
-            const error = e as Error;
-            message.reply(
-              `Oops, something bad happened
+						message.reply(`Please connect your OBS to \`${host}\``);
+						output.once("data", () => {
+							message.reply("Media stream found. Starting playback...");
+						});
+						await playStream(output, streamer, {
+							readrateInitialBurst: 10,
+						});
+					} catch (e) {
+						const error = e as Error;
+						message.reply(
+							`Oops, something bad happened
 \`\`\`
 ${error.message}
-\`\`\``
-            )
-          }
-        }
-      ),
+\`\`\``,
+						);
+					}
+				},
+			),
 
-      createCommand(
-        new Command("stop"),
-        () => {
-          playback?.kill("SIGTERM");
-        }
-      ),
+			createCommand(
+				new Command("yt-dlp")
+					.description("Play a video using yt-dlp")
+					.argument("<url>", "The url to play")
+					.option("--list-formats", "List all the formats in this video")
+					.option(
+						"--format <format>",
+						"The format to use. If not specified, use yt-dlp default",
+					),
+				async (message, args, opts) => {
+					const url = args[0];
+					await joinRoom(streamer, message);
+					playback?.kill("SIGTERM");
 
-      createCommand(
-        new Command("disconnect"),
-        () => {
-          playback?.kill("SIGTERM");
-          streamer.leaveVoice();
-        }
-      )
-    ]
-  }
+					try {
+						const { command, output } = ytdlp.ytdlp(url, opts.format, {
+							height: 1080,
+						});
+
+						command.on("error", ffmpegErrorHandler);
+						command.on("stderr", (line) => console.log(line));
+						playback = command;
+
+						await playStream(output, streamer);
+					} catch (e) {
+						const error = e as Error;
+						message.reply(
+							`Oops, something bad happened
+\`\`\`
+${error.message}
+\`\`\``,
+						);
+					}
+				},
+			),
+
+			createCommand(new Command("stop"), () => {
+				playback?.kill("SIGTERM");
+			}),
+
+			createCommand(new Command("disconnect"), () => {
+				playback?.kill("SIGTERM");
+				streamer.leaveVoice();
+			}),
+		];
+	},
 } satisfies Module;
---
+++ src/modules/stream/yt-dlp.ts
@@ -0,0 +1,21 @@
+import { $ } from "execa";
+import { NewApi } from "@dank074/discord-video-stream";
+
+export interface YtdlpFormat {
+    format_id: string,
+    ext: string,
+    resolution: string | null,
+    fps?: number | null
+}
+
+export async function getFormats(link: string)
+{
+    const result = (await $`yt-dlp --print "%(formats)#j" ${link}`).stdout;
+    return JSON.parse(result) as YtdlpFormat[];
+}
+
+export function ytdlp(link: string, format?: string, encoderOptions?: Partial<NewApi.EncoderOptions>)
+{
+    const stream = $`yt-dlp ${format ? "--format " + format : ""} -o - ${link}`.stdout;
+    return NewApi.prepareStream(stream, encoderOptions);
+}
