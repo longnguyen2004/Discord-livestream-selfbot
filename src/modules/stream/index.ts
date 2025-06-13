9b7ee85e0d6c4f87d093840975cdf922938d549e
Long Nguyen <nguyen.long.908132@gmail.com>
2025-06-13 19:45:23 +0700

    Added playlist functionality

--- src/modules/stream/index.ts
+++ src/modules/stream/index.ts
@@ -3,12 +3,12 @@ import { prepareStream, playStream, Streamer, type Controller } from "@dank074/d
 import * as Ingestor from "./input/ffmpegIngest.js";
 import * as ytdlp from "./input/yt-dlp.js";
 import { createCommand } from "../index.js";
-import { StageChannel } from "discord.js-selfbot-v13";
+import { MessageFlags, StageChannel } from "discord.js-selfbot-v13";
 
 import type { Module } from "../index.js";
 import type { Message } from "discord.js-selfbot-v13";
 
-async function joinRoom(
+async function joinRoomIfNeeded(
   streamer: Streamer,
   message: Message,
   optionalRoom?: string,
@@ -45,6 +45,57 @@ async function joinRoom(
   return true;
 }
 
+type StreamItem = {
+  controller: Controller,
+  promise: Promise<unknown>
+}
+type QueueItem = {
+  info: string,
+  stream: (abort: AbortController) => Promise<StreamItem>
+}
+
+class Playlist {
+  private _items: QueueItem[] = [];
+  private _current?: StreamItem;
+  private _abort?: AbortController;
+  private _processing = false;
+
+  private async processQueue() {
+    this._processing = true;
+    let next: QueueItem | undefined;
+    while (next = this._items.shift()) {
+      try {
+        this._abort?.abort();
+        this._abort = new AbortController();
+        this._current = await next.stream(this._abort);
+        await this._current.promise;
+      }
+      catch {
+      }
+    }
+    this._processing = false;
+    this._current = undefined;
+  }
+  queue(queueItem: QueueItem) {
+    this._items.push(queueItem);
+    if (!this._processing)
+      this.processQueue();
+  }
+  skip() {
+    this._abort?.abort();
+  }
+  stop() {
+    this._items = [];
+    this.skip();
+  }
+  get items() {
+    return this._items;
+  }
+  get current() {
+    return this._current;
+  }
+}
+
 function errorHandler(err: Error, message: Message) {
   if (err.name === "AbortError") return;
   message.reply(`Oops, something bad happened
@@ -56,8 +107,7 @@ ${err.message}
 function addCommonStreamOptions<
   Args extends unknown[],
   Opts extends Record<string, unknown>
->(command: Command<Args, Opts>)
-{
+>(command: Command<Args, Opts>) {
   return command
     .option(
       "--room <id>",
@@ -75,8 +125,7 @@ export default {
     const streamer = new Streamer(bot.client, {
       forceChacha20Encryption: true,
     });
-    let abortController: AbortController;
-    let streamController: Controller | null = null;
+    const playlist = new Playlist();
     return [
       createCommand(
         addCommonStreamOptions(
@@ -88,33 +137,41 @@ export default {
         ),
         async (message, args, opts) => {
           const url = args[0];
-          if (!(await joinRoom(streamer, message, opts.room))) return;
-          abortController?.abort();
-          abortController = new AbortController();
-          try {
-            const { command, output, controller } = prepareStream(
-              url,
-              {
-                noTranscoding: !!opts.copy,
-              },
-              abortController.signal,
-            );
-            streamController = controller;
+          if (!(await joinRoomIfNeeded(streamer, message, opts.room))) return;
+          playlist.queue({
+            info: args[0],
+            stream: async (abort) => {
+              message.reply({
+                content: `Now playing \`${args[0]}\``,
+                flags: MessageFlags.FLAGS.SUPPRESS_NOTIFICATIONS
+              })
+              try {
+                const { command, output, controller } = prepareStream(
+                  url,
+                  {
+                    noTranscoding: !!opts.copy,
+                  },
+                  abort.signal,
+                );
 
-            await playStream(
-              output,
-              streamer,
-              {
-                readrateInitialBurst: opts.livestream ? 10 : undefined,
-                streamPreview: opts.preview
-              },
-              abortController.signal,
-            );
-          } catch (e) {
-            errorHandler(e as Error, message);
-          } finally {
-            streamController = null;
-          }
+                const promise = playStream(
+                  output,
+                  streamer,
+                  {
+                    readrateInitialBurst: opts.livestream ? 10 : undefined,
+                    streamPreview: opts.preview
+                  },
+                  abort.signal,
+                );
+
+                return { controller, promise }
+              } catch (e) {
+                errorHandler(e as Error, message);
+                throw e;
+              }
+            }
+          });
+          message.reply(`Added \`${args[0]}\` to the queue`);
         },
       ),
 
@@ -134,40 +191,56 @@ export default {
             ),
         ),
         async (message, args, opts) => {
-          if (!(await joinRoom(streamer, message, opts.room))) return;
-          abortController?.abort();
-          abortController = new AbortController();
-          try {
-            const ingestor = {
-              srt: Ingestor.ingestSrt,
-              rtmp: Ingestor.ingestRtmp,
-              rist: Ingestor.ingestRist,
-            } as const;
-            const { command, output, host } = ingestor[opts.protocol](
-              opts.port,
-              abortController.signal,
-            );
-            abortController?.abort();
-            abortController = new AbortController();
+          if (!(await joinRoomIfNeeded(streamer, message, opts.room))) return;
+          playlist.queue({
+            info: "OBS stream",
+            stream: async (abort) => {
+              message.reply({
+                content: "Now playing OBS stream",
+                flags: MessageFlags.FLAGS.SUPPRESS_NOTIFICATIONS
+              })
+              try {
+                const ingestor = {
+                  srt: Ingestor.ingestSrt,
+                  rtmp: Ingestor.ingestRtmp,
+                  rist: Ingestor.ingestRist,
+                } as const;
+                const { command, output, host } = ingestor[opts.protocol](
+                  opts.port,
+                  abort.signal,
+                );
 
-            command.ffmpeg.on("stderr", (line) => console.log(line));
+                command.ffmpeg.on("stderr", (line) => console.log(line));
 
-            message.reply(`Please connect your OBS to \`${host}\``);
-            output.once("data", () => {
-              message.reply("Media stream found. Starting playback...");
-            });
-            await playStream(
-              output,
-              streamer,
-              {
-                readrateInitialBurst: 10,
-                streamPreview: opts.preview
-              },
-              abortController.signal,
-            );
-          } catch (e) {
-            errorHandler(e as Error, message);
-          }
+                message.reply(`Please connect your OBS to \`${host}\``);
+                output.once("data", () => {
+                  message.reply("Media stream found. Starting playback...");
+                });
+                const promise = playStream(
+                  output,
+                  streamer,
+                  {
+                    readrateInitialBurst: 10,
+                    streamPreview: opts.preview
+                  },
+                  abort.signal,
+                );
+                const controller = {
+                  get volume() {
+                    return 1;
+                  },
+                  async setVolume() {
+                    throw new Error("Setting volume for OBS streams isn't allowed at the moment");
+                  }
+                }
+                return { controller, promise }
+              } catch (e) {
+                errorHandler(e as Error, message);
+                throw e;
+              }
+            }
+          });
+          message.reply("Added OBS stream to the queue");
         },
       ),
 
@@ -200,35 +273,44 @@ export default {
             message.reply(reply);
             return;
           }
-          if (!(await joinRoom(streamer, message, opts.room))) return;
-          abortController?.abort();
-          abortController = new AbortController();
+          if (!(await joinRoomIfNeeded(streamer, message, opts.room))) return;
 
-          try {
-            const { command, output, controller } = ytdlp.ytdlp(
-              url,
-              opts.format,
-              {
-                h26xPreset: "superfast",
-                height: opts.height,
-                bitrateVideo: 5000,
-                bitrateVideoMax: 7500,
-              },
-              abortController.signal,
-            );
-            streamController = controller;
-            command.ffmpeg.on("stderr", (line) => console.log(line));
-            await playStream(
-              output,
-              streamer,
-              {
-                streamPreview: opts.preview
-              },
-              abortController.signal,
-            );
-          } catch (e) {
-            errorHandler(e as Error, message);
-          }
+          playlist.queue({
+            info: args[0],
+            stream: async (abort) => {
+              message.reply({
+                content: `Now playing \`${args[0]}\``,
+                flags: MessageFlags.FLAGS.SUPPRESS_NOTIFICATIONS
+              })
+              try {
+                const { command, output, controller } = ytdlp.ytdlp(
+                  url,
+                  opts.format,
+                  {
+                    h26xPreset: "superfast",
+                    height: opts.height,
+                    bitrateVideo: 5000,
+                    bitrateVideoMax: 7500,
+                  },
+                  abort.signal,
+                );
+                command.ffmpeg.on("stderr", (line) => console.log(line));
+                const promise = playStream(
+                  output,
+                  streamer,
+                  {
+                    streamPreview: opts.preview
+                  },
+                  abort.signal,
+                );
+                return { controller, promise }
+              } catch (e) {
+                errorHandler(e as Error, message);
+                throw e
+              }
+            }
+          });
+          message.reply(`Added \`${args[0]}\` to the queue`);
         },
       ),
 
@@ -237,35 +319,54 @@ export default {
           .description("Adjust the stream volume, or get the current volume")
           .argument("[value]", "The new stream volume (must be non-negative)"),
         async (msg, args) => {
-          if (!streamController)
-          {
+          if (!playlist.current) {
             msg.reply("No stream is currently running");
             return;
           }
-          if (!args[0])
-          {
-            msg.reply(`Current volume: ${streamController.volume}`);
+          const { controller } = playlist.current;
+          if (!args[0]) {
+            msg.reply(`Current volume: ${controller.volume}`);
             return;
           }
           const volume = Number.parseFloat(args[0]);
-          if (!Number.isFinite(volume))
-          {
+          if (!Number.isFinite(volume)) {
             msg.reply("Invalid number");
             return;
           }
-          if (await streamController.setVolume(volume))
-            msg.reply("Set volume successful");
-          else
-            msg.reply("Set volume unsuccessful");
+          try {
+            if (await controller.setVolume(volume))
+              msg.reply("Set volume successful");
+            else
+              msg.reply("Set volume unsuccessful");
+          }
+          catch (e) {
+            msg.reply(`Set volume unsuccessful: \`${(e as Error).message}\``)
+          }
         }
       ),
 
+      createCommand(
+        new Command("queue").description("View the queue"),
+        async(message) => {
+          if (!playlist.items.length)
+            return message.reply("There are no items in the queue");
+          const { length } = playlist.items;
+          let content = `There are ${length} ${length === 1 ? "item" : "items"} in the queue`;
+          let i = 1;
+          for (const item of playlist.items)
+            content += `\n${i++}. \`${item.info}\``;
+          return message.reply(content);
+        }),
+      createCommand(new Command("skip"), () => {
+        playlist.skip();
+      }),
+
       createCommand(new Command("stop"), () => {
-        abortController?.abort();
+        playlist.stop();
       }),
 
       createCommand(new Command("disconnect"), () => {
-        abortController?.abort();
+        playlist.stop();
         streamer.leaveVoice();
       }),
     ];
